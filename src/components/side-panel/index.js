b88f98b67ae4939deaea23cb01e7f26224fc1214
santosneilaldrin@gmail.com <santosneilaldrin@gmail.com>
2020-09-10 15:52:09 +0800

    updated home

--- src/components/side-panel/index.js
+++ src/components/side-panel/index.js
@@ -4,11 +4,17 @@ import style from './style';
 
 const SidePanel = () => (
 	<div class={style.sidePanel}>
-		<Link href="/"><h1>Neil Aldrin Santos</h1></Link>
-		<nav>
+		<div>
+			<div class={style.image} />
+			<Link href="/"><h1>Neil Aldrin Santos</h1></Link>
+			<p>Software developer by trade.</p>	
+		</div>
+		<div>
+		<nav >
 			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
 			<Link activeClassName={style.active} href="/contact">Contact me</Link>
 		</nav>
+		</div>
 	</div>
 );
 
