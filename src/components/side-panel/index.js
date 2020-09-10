05fd2673ebef915eeb2e4e23a994251f238892af
santosneilaldrin@gmail.com <santosneilaldrin@gmail.com>
2020-09-10 22:22:14 +0800

    updated sidepanel element layouts. added font awesome

--- src/components/side-panel/index.js
+++ src/components/side-panel/index.js
@@ -4,16 +4,29 @@ import style from './style';
 
 const SidePanel = () => (
 	<div class={style.sidePanel}>
-		<div>
+		<div class={style.profileBio}>
 			<div class={style.image} />
 			<Link href="/"><h1>Neil Aldrin Santos</h1></Link>
 			<p>Software developer by trade.</p>	
+			<div>
+				<a target="_blank" href="https://www.linkedin.com/in/neil-aldrin-santos-9a67676b/">
+					<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+				</a>
+				<a target="_blank" href="https://github.com/neil445">
+					<i class="fa fa-github" aria-hidden="true"></i>
+				</a>
+				<a target="_blank" href="https://dev.to/neil445">
+					<i class="fa fa-file-text" aria-hidden="true"></i>
+				</a>
+			</div>
 		</div>
-		<div>
-		<nav >
-			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
-			<Link activeClassName={style.active} href="/contact">Contact me</Link>
-		</nav>
+		<div>	
+			<nav>
+				<Link activeClassName={style.active} href="/">About me</Link>
+				<Link activeClassName={style.active} href="/blogs">Write ups</Link>
+				<Link activeClassName={style.active} href="/contact">Contact me</Link>
+				<Link activeClassName={style.active} href="/">Hire me!</Link>
+			</nav>
 		</div>
 	</div>
 );
